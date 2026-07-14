4dc17bfdda3bb6a659d23bdb0539e98d76e8fa14
Ash-exwo <aswathygr61@gmail.com>
2026-07-15 03:05:47 +0530

    View Booking v1

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom"
 import AddBooking from "./components/AddBooking"
+import ViewBooking from "./components/ViewBooking"
 
 function App() {
 
@@ -8,6 +9,7 @@ function App() {
     <BrowserRouter>
     <Routes>
       <Route path="/add-booking" element={<AddBooking/>}/>
+      <Route path="/view-booking" element={<ViewBooking/>}/>
     </Routes>
     </BrowserRouter>
     </>
